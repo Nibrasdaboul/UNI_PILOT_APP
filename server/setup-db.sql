7578ef032d731cd0b9c1ccb44746bc4b96b4c3a9
Nibrasdaboul <137706385+Nibrasdaboul@users.noreply.github.com>
2026-02-28 22:17:45 +0300

    Delete server directory

--- server/setup-db.sql
+++
@@ -1,5 +0,0 @@
--- إنشاء مستخدم وقاعدة بيانات UniPilot
--- غيّر كلمة المرور 'UniPilot123' لو حاب
-
-CREATE USER unipilot_user WITH PASSWORD 'UniPilot123';
-CREATE DATABASE unipilot OWNER unipilot_user;
